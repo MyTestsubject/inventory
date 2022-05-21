c80aaf85e0d17f42cf400057518ee13b1d0388c7
MyTestsubject <64662693+MyTestsubject@users.noreply.github.com>
2022-05-20 23:52:32 -0400

    pt 6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import './App.css';
-import { PropTypes } from 'prop-types';
-import Info from "./info"
+import AddItem from './AddItem';
+import SearchBar from './SearchBar';
 
 function App() {
+  const [filters, setFilters] = useState({})
+  const [data, setData] = useState({items: [] });
+
+  const updateFilters = (searchParams)=> {
+    setFilters(searchParams);
+  }
+
+  const addItemToData = (item) => {
+    let items = data["items"];
+    items.push(item);
+    setData({items: items});
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <Info title="Inventory"/>
+      <SearchBar updateSearchParams={ updateFilters } />
+      <AddItem addItem={ addItemToData } />
     </div>
   );
 }
+
+ 
 
 export default App;
